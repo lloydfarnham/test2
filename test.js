9c982bc4d21cfadaf69fd5b9a6ccca04fbc4abdf
lloydfarnham <lloyd1996@gmail.com>
2023-11-16 18:53:40 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,26 @@
-var Timer;var TotalSeconds;function CreateTimer(TimerID,Time){Timer=document.getElementById(TimerID);TotalSeconds=Time;Timer.innerHTML=TotalSeconds;window.setTimeout("CountDown()",10);}
-function CountDown(){if(TotalSeconds<=1)
-{location.reload(true);return;}
-TotalSeconds-=1;Timer.innerHTML=TotalSeconds;window.setTimeout("CountDown()",10);}
+var Timer;
+var TotalSeconds;
+
+function CreateTimer(TimerID, Time) {
+  Timer = document.getElementById(TimerID);
+  TotalSeconds = Time;
+  Timer.innerHTML = TotalSeconds;
+  window.setTimeout(CountDown, 10);
+}
+
+function CountDown() {
+  if (TotalSeconds <= 1) {
+    location.reload(true);
+    return;
+  }
+  TotalSeconds -= 1;
+  Timer.innerHTML = TotalSeconds;
+  window.setTimeout(CountDown, 10);
+}
+
+window.onload = function() {
+  setTimeout(function() {
+    location.reload(true);
+  }, 100);
+}
+
